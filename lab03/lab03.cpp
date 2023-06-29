503dae4a57e655bf2fd210516286a1e32e2928d5
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2023-06-29 18:39:41 +0300

    code: add svg scaling

--- lab03/lab03.cpp
+++ lab03/lab03.cpp
@@ -112,7 +112,7 @@ void svg_text(double left, double baseline, string text) {
 	cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
 }
 
-void svg_rect(double x, double y, double width, double height, string stroke, string fill) {
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
 	cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
 }
 
@@ -127,10 +127,30 @@ void show_histogram_svg(const vector<size_t>& bins) {
 
 	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 	double top = 0;
+
+	size_t max_count = 0;
 	for (size_t bin : bins) {
-		const double bin_width = BLOCK_WIDTH * bin;
+		if (bin > max_count) {
+			max_count = bin;
+		}
+	}
+
+	double bin_width;
+	for (size_t bin : bins) {
+		if (max_count > MAX_ASTERISK) {
+			if (bin > MAX_ASTERISK) {
+				bin_width = MAX_ASTERISK * 1.0 * BLOCK_WIDTH;
+			}
+			else {
+				bin_width = MAX_ASTERISK * (static_cast<double>(bin) / max_count) * BLOCK_WIDTH;
+			}
+		}
+		else {
+			bin_width = BLOCK_WIDTH * bin;
+		}
+
 		svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
 		top += BIN_HEIGHT;
 	}
 	svg_end();	
