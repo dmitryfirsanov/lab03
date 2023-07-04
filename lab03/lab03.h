65cbb2a0db72a7fd167f3ee11598374161e42034
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2023-07-04 10:17:48 +0300

    code: lil fixer

--- lab03/lab03.h
+++ lab03/lab03.h
@@ -26,5 +26,5 @@ void show_histogram_text(const vector<size_t>& bins);
 void svg_begin(double width, double height);
 void svg_end();
 void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
+void svg_rect(double x, double y, double width, double height, string stroke, string fill = "black");
 void show_histogram_svg(const vector<size_t>& bins, string stroke);
